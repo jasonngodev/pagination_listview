d33e642cc909a3340fdd37553e47039444a5e525
JasonNgo <thangloi@iMac-cua-TLG.local>
2021-04-20 09:23:33 +0700

    update

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:http/http.dart' as http;
 
@@ -10,14 +11,24 @@ class APIService {
     //String url = "/GetRadios?pageSize=15&pageNumber=$pageNumber";
     String url = "?size=10&page=$pageNumber";
     print("URL : $url");
-    final response = await http.get(Uri.parse(Config.apiURL + url));
-    if (response.statusCode == 200) {
-      print(response.body);
-      return DataModel.fromJson(
-        json.decode(response.body),
-      );
-    } else {
-      throw Exception('Failed to load data!');
+    try {
+      final response = await http.get(Uri.parse(Config.apiURL + url));
+      if (response.statusCode == 200) {
+        print(response.body);
+        return DataModel.fromJson(
+          json.decode(response.body),
+        );
+      } else {
+        throw Exception('Failed to load data!');
+      }
+    } catch (e) {
+      if (e is IOException) {
+        // return CountriesData.withError(
+        //     'Please check your internet connection.');
+      } else {
+        print(e.toString());
+        // return CountriesData.withError('Something went wrong.');
+      }
     }
   }
 }
